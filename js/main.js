01e84d5c744b92f021c3f29a6dd5212042ddee09
lucaslost6951 <lucasalemao.ns@gmail.com>
2016-03-17 14:18:23 +0000

    inicio do projeto

---
+++ js/main.js
@@ -0,0 +1,271 @@
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
+
+function main() {
+
+(function () {
+   'use strict';
+
+   /* ==============================================
+  	Testimonial Slider
+  	=============================================== */ 
+
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+	  
+	  
+	  $(document).ready(function(){
+    $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+     
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+});
+
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+
+  	$(document).ready(function() {
+  	  $("#team").owlCarousel({
+  	 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 4],
+				        [1400, 4],
+				        [1600, 4]
+				      ],
+  	  });
+
+  	  $("#clients").owlCarousel({
+  	 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 5],
+				        [1400, 5],
+				        [1600, 5]
+				      ],
+  	  });
+
+      $("#testimonial").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});
+
+  	/*====================================
+    Portfolio de audio
+    ======================================*/
+$(document).ready(function(){
+    $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+     
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+});
+
+/*====================================
+    Portfolio videos
+    ======================================*/
+$(document).ready(function(){
+    $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+        var alturaTela = $(document).height();
+        var larguraTela = $(windowVideos).width();
+     
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(windowVideos).width() /2) - ( $(id).width() / 2 );
+        var top = ($(windowVideos).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".windowVideos").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".windowVideos").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+        var alturaTela = $(document).height();
+        var larguraTela = $(windowVideos).width();
+     
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(windowVideos).width() /2) - ( $(id).width() / 2 );
+        var top = ($(windowVideos).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".windowImg").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".windowImg").hide();
+    });
+});
+
+
+
+  	/*====================================
+    Portfolio Isotope Filter
+    ======================================*/
+    
+    
+    $(window).load(function() {
+        var $container = $('#lightbox');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+
+
+}());
+
+
+}
+main();
